9372452dd9f338800ef53699fb0847f97c523f70
MaherSaleem <the-maher@live.com>
2020-09-02 17:35:53 +0300

    Moved suffixes to LightStemmer Class

--- spec/porter_stemmer_ar_spec.js
+++ spec/porter_stemmer_ar_spec.js
@@ -4,7 +4,7 @@ var stemmer = require('../lib/natural/stemmers/porter_stemmer_ar');
 
 describe('porter_stemmer', function () {
   it('dummy test', function () {
-    stemmer.stem("والملعب");
+    stemmer.stem("كالمعلمات");
     // expect(stemmer.stem("يل؟؟ــــّعب??!؟؟لاــ٠")).toBe("يلعب0");
   });
 });
