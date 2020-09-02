4b7b42bd9b9ac219e32ece928eb912485fa8ab4e
MaherSaleem <the-maher@live.com>
2020-09-02 17:02:46 +0300

    Moved Prefixes to LightStemmer Class

--- spec/porter_stemmer_ar_spec.js
+++ spec/porter_stemmer_ar_spec.js
@@ -4,7 +4,7 @@ var stemmer = require('../lib/natural/stemmers/porter_stemmer_ar');
 
 describe('porter_stemmer', function () {
   it('dummy test', function () {
-    stemmer.stem("ببباب");
+    stemmer.stem("والملعب");
     // expect(stemmer.stem("يل؟؟ــــّعب??!؟؟لاــ٠")).toBe("يلعب0");
   });
 });
